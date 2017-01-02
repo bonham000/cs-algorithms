f17157e0256e7b6ab13fcd1e8aa0350fe34e2373
Sean Smith <sean.smith.2009@gmail.com>
2017-01-02 12:55:24 -0800

    added objects challenge 08 draft

---
+++ data-structures/fcc_basic/objects/08.js
@@ -0,0 +1,79 @@
+document.write(`
+<b>Title:</b> Modifying an Array Stored in an Object
+`)
+
+document.write(`<br><br>
+<b>Description:</b> Now you've seen all the basic operations for JavaScript objects. You can add, modify, and remove key-value pairs, check if keys exists, and
+iterate over all the keys in an object. As you continue learning JavaScript you will see even more versatile applications of objects. Additionally, the
+optional Advanced Data Structures lessons later in the curriculum also cover the ES6 <code>Map</code> and <code>Set</code> objects, both of which are similar
+to ordinary objects but provide some additional features. Now that you've learned the basic of arrays and objects, you're ready to put this knowledge to use
+solving some algorithms!
+`);
+
+document.write(`<br><br>
+<b>Instructions:</b> Take a look at the object we've provided in the code editor. The <code>user</code> object contins three keys. The <code>data</code> key
+contains two object keys, one of which contains an array of friends. From this you can see how flexible objects are as data structures. 
+`);
+
+// SEED CODE
+let user = {
+	name: 'Kenneth',
+	age: 28,
+	data: {
+		username: 'kennethCodesAllDay',
+		joinDate: 'March 26, 2016',
+		organization: 'Free Code Camp',
+		friends: [
+			'Sam',
+			'Kira',
+			'Tomo'
+		],
+		location: {
+			city: 'San Francisco',
+			state: 'CA',
+			country: 'USA'
+		}
+	}
+};
+
+function addFriend(user, friend) {
+	// change code below this line	
+
+	// change code above this line
+};
+
+// SOLUTION CODE
+let user = {
+	name: 'Kenneth',
+	age: 28,
+	data: {
+		username: 'kennethCodesAllDay',
+		joinDate: 'March 26, 2016',
+		organization: 'Free Code Camp',
+		friends: [
+			'Sam',
+			'Kira',
+			'Tomo'
+		],
+		location: {
+			city: 'San Francisco',
+			state: 'CA',
+			country: 'USA'
+		}
+	}
+};
+
+function addFriend(user, friend) {
+	user.data.friends.push(friend);
+	return user;
+};
+
+// TESTS
+console.clear();
+
+console.assert('name' in user && 'age' in user && 'data' in user, 'The user object has name, age, and data keys');
+console.assert((function() { let L1 = user.data.friends.length; addFriend(user, 'Sean'); let L2 = user.data.friends.length; return (L2 === L1 + 1); })(), 'The addFriend function accepts a user object and a friend as arguments and adds the friend to the array of friends in the user object.');
+
+
+
+
