5e95a1a4150227978431c3217f9c8803ebf0fdd0
Sean Smith <sean.smith.2009@gmail.com>
2017-01-14 23:46:39 -0800

    added game of life solution

--- algorithms/leet-code/game-of-life.js
+++ algorithms/leet-code/game-of-life.js
@@ -1,28 +1,4 @@
-
-"use strict"
-
-// performance timer:
-const timeFn = (fn, args) => {
-	let timeStart = performance.now();
-	let result = fn(args);
-	let timeEnd = performance.now();
-	console.log(`${fn.prototype.constructor.name} took ${(timeEnd - timeStart).toFixed(6) * 1000} microseconds to run and returned:`);
-	console.log(result);
-};
-
-function isSorted(arr) {
-	let check = (i) => (i == arr.length - 1) ? true : (arr[i] > arr[i + 1]) ? false : check(i + 1);
-	return check(0);
-};
-
-// generate a random array:
-var R = [];
-(function gen(n = 5) {
-	while (R.length < n) R.push(+(Math.random() * 10).toFixed());
-	return R;
-})(15)
-
-var sorted = (A) => A.sort((a, b) => a - b);
+// given a matix representing the game of life board, modify it and return the next state in place:
 
 function gameOfLife(board) {
 	let nextState = [];
@@ -61,17 +37,3 @@ function gameOfLife(board) {
 		}
 	}
 }
-
-console.clear();
-
-var N = [
-	[0,0,0,0],
-	[0,1,1,0],
-	[0,1,1,0],
-	[0,0,0,0]
-];
-
-console.log(gameOfLife(N));
-
-
-
