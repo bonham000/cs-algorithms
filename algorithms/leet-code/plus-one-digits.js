eed0ddffaba4606ae7946e6eb7efeaa7e0304125
Sean Smith <sean.smith.2009@gmail.com>
2017-01-15 20:38:05 -0800

    added plus-one solution

--- algorithms/leet-code/plus-one-digits.js
+++ algorithms/leet-code/plus-one-digits.js
@@ -1,11 +1,8 @@
-
-"use strict";
-
 var plusOne = function(digits) {
 
   var curr = 1;
 
-  while(curr < digits.length) {
+  while(curr <= digits.length) {
 
   	if (digits[digits.length - curr] == 9) {
   		digits[digits.length - curr] = 0;
@@ -20,7 +17,3 @@ var plusOne = function(digits) {
   return [1].concat(digits);
 
 };
-
-var num = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3];
-var nines = [9,9,9];
-console.log(plusOne([0]));
