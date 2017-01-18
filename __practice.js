28461a96edaa418aeb620729bb84ec90691fc103
Sean Smith <sean.smith.2009@gmail.com>
2017-01-17 16:54:08 -0800

    updated promise-all solution

--- __practice.js
+++ __practice.js
@@ -32,58 +32,55 @@ var sorted = (A) => A.sort((a, b) => a - b);
 
 console.clear();
 
-// helper function to generate a promise which may resolve or reject on a random basis
-function generatePromise() {
+function genPromise() {
 	return new Promise((resolve, reject) => {
-		var resolved = false;
+		let resolved = false;
+		let time = Math.round(2000 - Math.random() * 1000);
 		setTimeout(() => {
 			resolved = true;
-			resolve('Great!');
-		}, Math.floor(2000 - (Math.random() * 1000)));
+			resolve(`Resolved in ${time} milliseconds!`);
+		}, time);
 		setTimeout(() => {
-			if (!resolved) reject('rejected...');
-		}, 1900);
-	});
-}
+			if (!resolved) reject('Rejected...');
+		}, 1950);
+	})
+};
 
-// promise.all implementation
-// takes an array of promises as input and returns an array of the fulfilled values if all resolve
-// if any promises reject immediately rejects and reports which promise failed
 function promiseAll(promises) {
-	return new Promise(function(resolve, reject) {
-		var results = [];
-		var unresolved = promises.length;
-		function handlePromise(promise, idx) {
-			promise.then(response => {
-				unresolved--;
-				results[idx] = `Promise ${idx + 1} fulfilled: ${response}`;
-				if (unresolved === 0) resolve(results);
+	return new Promise((resolve, reject) => {
+		var fulfillment = [];
+		var remaining = promises.length;
+		function handlePromise(promise, i) {
+			promise.then(result => {
+				remaining--;
+				fulfillment[i] = `Promise ${i + 1}: ${result}`
+				if (remaining === 0) resolve(fulfillment);
 			}).catch(err => {
-				results[idx] = err;
-				reject(`Promise ${idx} failed...`);
+				reject(`Promise #${i + 1} failed: ${err}`);
 			});
-		}
+		};
 		promises.forEach((promise, i) => handlePromise(promise, i));
 	});
-}
+};
 
-// generate an array of example promises
 var promises = [];
 var n = 0;
-while (n < 5) {
-	promises.push(generatePromise());
+while (n < 10) {
+	promises.push(genPromise());
 	n++;
-}
+};
 
-var allPromises = promiseAll(promises)
+var allPromises = promiseAll(promises);
 
-allPromises.then(fulfillment => {
-	// logs array of all fulfilled values if all fulfill
-	fulfillment.forEach(msg => console.log(msg));
+allPromises.then(res => {
+	res.forEach(data => console.log(data));
 }).catch(err => {
-	// if any fail, immediately rejects and reports which failed
-	console.log(err);
+	console.warn(err);
 });
+
+
+
+
 
 
 
