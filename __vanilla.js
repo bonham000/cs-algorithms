e5f94e6b04f94ad881a9dbc6f914c16d95a16afd
Sean Smith <sean.smith.2009@gmail.com>
2017-01-18 17:22:51 -0800

    updated promises example

--- __vanilla.js
+++ __vanilla.js
@@ -1,32 +1,55 @@
 
 "use strict";
 
-var plusOne = function(digits) {
+console.clear();
 
-  var curr = 1;
-
-  while(curr <= digits.length) {
-
-  	if (digits[digits.length - curr] == 9) {
-  		digits[digits.length - curr] = 0;
-  		curr++;
-  	} else {
-			digits[digits.length - curr] = digits[digits.length - curr] + 1;
-			return digits;
-  	}
-  }
-
-  digits[digits.length - curr] = 0;
-  return [1].concat(digits);
-
+function generatePromiseArray(n) {
+	var promises = [];
+	var curr = 0;
+	while (curr < n) {
+		var promise = new Promise((resolve, reject) => {
+			var resolved = false;
+			setTimeout(() => {
+				resolved = true;
+				resolve('promise fulfilled');
+			}, (2000 - Math.random() * 1000));
+			setTimeout(() => {
+				if (!resolved) reject('promise rejected');
+			}, 1900);
+		});
+		promises.push(promise);
+		curr++;
+	}
+	return promises;
 };
 
-console.log(plusOne([0]));
+function handleAllPromises(array) {
+	return new Promise((resolve, reject) => {
+		var results = [];
+		var fulfilled = 0;
+		function handlePromise(promise, idx) {
+			promise.then((data) => {
+				fulfilled++;
+				results[idx] = ('Promise ' + (idx + 1) + ' fulfilled: ' + data);
+				if (fulfilled === array.length) resolve(results);
+			}).catch((err) => {
+				fulfilled++;
+				results[idx] = ('Promise ' + (idx + 1) + ' failed: ' + err);
+				if (fulfilled === array.length) resolve(results);
+			});
+		};
+		array.forEach((promise, idx) => {
+			results.push(handlePromise(promise, idx));
+		});
+	});
+}
 
-
-
-
-
+handleAllPromises(generatePromiseArray(150)).then(res => {
+	console.log('All promises resolved!');
+	res.forEach((data) => console.log(data));
+}).catch(err => {
+	console.log(err);
+});
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-	entry: './__practice.js',
+	entry: './__vanilla.js',
 	output: {
 		filename: 'bundle.js'
 	},
